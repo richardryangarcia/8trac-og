9f59c1172f3768936996f3277a20f995c46b33bf
Zachary Rodriguez <zjrodr0709@hotmail.com>
2018-03-21 21:32:31 -0500

    Commented out code signup error

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,7 +38,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
         set_flash_message! :notice, :signed_up
         sign_up(resource_name, resource)
         respond_with resource, location: after_sign_up_path_for(resource)
-        user.setup_blockchain_instance
+        #user.setup_blockchain_instance
       else
         set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
